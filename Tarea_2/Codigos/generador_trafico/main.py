a25c9c25b294e953651472cc285bad9307500127
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2026-06-03 17:25:16 -0400

    Increase iterations in traffic simulation to 1000

--- Tarea_2/Codigos/generador_trafico/main.py
+++ Tarea_2/Codigos/generador_trafico/main.py
@@ -6,7 +6,7 @@ from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
 
 ZONAS = ["Z1", "Z2", "Z3", "Z4", "Z5"]
-QUERIES = ["q1", "q2", "q3", "q5"] # Q4 se trata distinto por recibir dos zonas [cite: 37]
+QUERIES = ["q1", "q2", "q3", "q5"] 
 TOPICO_PRINCIPAL = "consultas_geoespaciales"
 
 producer = None
@@ -33,7 +33,7 @@ def generar_consulta(modo="uniform"):
     
     return payload
 
-def simular_trafico(iteraciones=100, modo="zipf"):
+def simular_trafico(iteraciones=1000, modo="zipf"):
     print(f"--- Iniciando Simulación de Tráfico Asíncrono: Modo {modo.upper()} ---")
     for i in range(iteraciones):
         payload = generar_consulta(modo=modo)
@@ -50,23 +50,21 @@ def simular_trafico(iteraciones=100, modo="zipf"):
 if __name__ == "__main__":
     print("Iniciando fase de conexión con Kafka...")
     
-    # BUCLE INTELIGENTE: Reintenta hasta que Kafka esté UP de verdad
     while True:
         try:
             print("Intentando conectar con el Broker de Kafka (kafka:29092)...")
             producer = KafkaProducer(
                 bootstrap_servers=['kafka:29092'],
                 value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                request_timeout_ms=5000  # Tiempo de espera por intento
+                request_timeout_ms=5000  
             )
             print("[ÉXITO] ¡Conectado exitosamente al Broker de Kafka!")
-            break  # Rompe el bucle si se conecta con éxito
+            break  
         except NoBrokersAvailable:
             print("[ESPERA] El Broker de Kafka aún no está disponible. Reintentando en 5 segundos...")
             time.sleep(5)
     
-    # Una vez conectado, ejecuta la simulación
-    simular_trafico(iteraciones=100, modo="uniform")
+    simular_trafico(iteraciones=1000, modo="uniform")
     
     print("\nCambiando a modo Zipf en 5 segundos...\n")
     time.sleep(5)
